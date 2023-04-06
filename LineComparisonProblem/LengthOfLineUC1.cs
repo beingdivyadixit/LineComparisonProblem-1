06258cb242ed902ce7839986f041b2e6fb77dd6b
Divya <beingdivyadixit@gmail.com>
2023-04-06 07:18:56 +0530

    UC-1

---
+++ LineComparisonProblem/LengthOfLineUC1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class LengthOfLineUC1
+    {
+        public static void LengthUC1(double X1, double Y1, double X2, double Y2)
+        {
+            double X = Math.Pow((X2 - X1), 2);
+            double Y = Math.Pow((Y2 - Y1), 2);
+            double lengthOfLine = Math.Sqrt(X + Y);
+            Console.WriteLine("Length Of Line is :" + lengthOfLine);
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -7,6 +7,29 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Line Comparison Problem");
+            Console.WriteLine(" Enter 0 For Display Welcome to Line Comparison \n Enter 1 For Calculate the lenght of line based on points consisting of (X,Y) co-ordinates");
+            int option = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
+            switch (option)
+            {
+
+                case 0:
+                    Console.WriteLine(" Welcome to Line Comparison");
+                    break;
+                case 1:
+                    Console.WriteLine("Please Enter X1 & Y1 Co-Ordinates:");
+                    var X1 = Convert.ToDouble(Console.ReadLine());
+                    var Y1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Please Enter X2 & Y2 Co - Ordinates:");
+                    var X2 = Convert.ToDouble(Console.ReadLine());
+                    var Y2 = Convert.ToDouble(Console.ReadLine());
+                    LengthOfLineUC1.LengthUC1(X1, Y1, X2, Y2);
+                    break;
+                default:
+                    Console.WriteLine("Please Input Correct option");
+                    break;
+
+            }
         }
     }
 }
