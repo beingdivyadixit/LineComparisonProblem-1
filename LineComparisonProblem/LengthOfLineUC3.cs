27b88561afe7f873d6baa86b082c75165ab5c4eb
Divya <beingdivyadixit@gmail.com>
2023-04-06 07:27:14 +0530

    UC-3

---
+++ LineComparisonProblem/LengthOfLineUC3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisonProblem
+{
+    internal class LengthOfLineUC3
+    {
+        public static void LengthUC3(double X1, double Y1, double X2, double Y2, double X3, double Y3, double X4, double Y4)
+        {
+            double X = Math.Pow((X2 - X1), 2);
+            double Y = Math.Pow((Y2 - Y1), 2);
+            double lengthOfFirstLine = Math.Sqrt(X + Y);
+            double x = Math.Pow((X4 - X3), 2);
+            double y = Math.Pow((Y4 - Y3), 2);
+            double lengthOfSecondLine = Math.Sqrt(x + y);
+            if (lengthOfFirstLine == lengthOfSecondLine)
+            {
+                Console.WriteLine("Lines are Equal");
+            }
+            else if (lengthOfFirstLine > lengthOfSecondLine)
+            {
+                Console.WriteLine("First Line is Greater than Second Line");
+            }
+            else
+            {
+                Console.WriteLine("First Line is lesser than Second Line");
+            }
+        }
+    }
+}
--- LineComparisonProblem/Program.cs
+++ LineComparisonProblem/Program.cs
@@ -7,7 +7,7 @@ namespace LineComparisonProblem
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Line Comparison Problem");
-            Console.WriteLine("Enter 0 For Display Welcome to Line Comparison \n Enter 1 For Calculate the lenght of line based on points consisting of (X,Y) co-ordinates \n Enter 2 For Check The Equality Of Two line");
+            Console.WriteLine("Enter 0 For Display Welcome to Line Comparison \n Enter 1 For Calculate the lenght of line based on points consisting of (X,Y) co-ordinates \n Enter 2 For Check The Equality Of Two line \n Enter 3 For Comparing Two Lines");
             int option = Convert.ToInt32(Console.ReadLine());
             Random random = new Random();
             switch (option)
@@ -44,6 +44,23 @@ namespace LineComparisonProblem
                         var Y4 = Convert.ToDouble(Console.ReadLine());
                         LengthOfLineUC2.LengthUC2(X1, Y1, X2, Y2, X3, Y3, X4, Y4);
 
+                    }
+                    break;
+                case 3:
+                    {
+                        Console.WriteLine("Please Enter X1 & Y1 Co-Ordinates:");
+                        var X1 = Convert.ToDouble(Console.ReadLine());
+                        var Y1 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please Enter X2 & Y2 Co - Ordinates:");
+                        var X2 = Convert.ToDouble(Console.ReadLine());
+                        var Y2 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please Enter X3 & Y3 Co-Ordinates:");
+                        var X3 = Convert.ToDouble(Console.ReadLine());
+                        var Y3 = Convert.ToDouble(Console.ReadLine());
+                        Console.WriteLine("Please Enter X4 & Y4 Co - Ordinates:");
+                        var X4 = Convert.ToDouble(Console.ReadLine());
+                        var Y4 = Convert.ToDouble(Console.ReadLine());
+                        LengthOfLineUC3.LengthUC3(X1, Y1, X2, Y2, X3, Y3, X4, Y4);
                     }
                     break;
                 default:
